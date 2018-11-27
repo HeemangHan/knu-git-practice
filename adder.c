e155367650d992a12ecb1b26e5a41aad51736ef8
HeemangHan <peniewk@naver.com>
2018-11-27 23:36:31 +0900

    supper adder 기능 개발

--- adder.c
+++ adder.c
@@ -2,3 +2,8 @@ int adder(int a, int b) {
     printf("input a: %d input b: %d", a, b);
     return a + b;
 }
+
+int super_adder(int a, int b) {
+    printf("supper adder: %d\n", a, b);
+    return a + b;
+}
